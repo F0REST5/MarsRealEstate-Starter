9fad51b20012700ec822d6ad29d5316d78eab598
F0REST5 <73851205+F0REST5@users.noreply.github.com>
2022-12-12 16:52:56 +0100

    Connect web service with Retrofit

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -24,6 +24,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
 
+
 /**
  * This fragment shows the the status of the Mars real-estate web services transaction.
  */
